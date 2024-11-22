8b292a69ab08a5df03745a31d0cb288202552629
PuneethSunku <134520958+PuneethSunku@users.noreply.github.com>
2024-11-22 12:54:10 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './Login.css'; // Optional: Add your CSS styles here
+import './Login.css';
 import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Import eye icons
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+  const [showPassword, setShowPassword] = useState(false); //State to toggle password visibility
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
@@ -23,9 +23,9 @@ const Login = () => {
   
       console.log('Token:', token);
       console.log('isAdmin:', isAdmin);
-  
-      // Save token to localStorage (using the same key as in middleware)
+      
       localStorage.setItem('auth-token', token);
+      localStorage.setItem('isAdmin', isAdmin);
   
       // Redirect based on user role
       if (isAdmin) {
@@ -50,26 +50,11 @@ const Login = () => {
       <h2>Log In</h2>
       <form onSubmit={handleLogin}>
       <div className="password-input-container">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         </div>
         <div className="password-input-container">
-          <input
-            type={showPassword ? 'text' : 'password'} // Toggle between password and text
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <span
-            className="toggle-password"
-            onClick={() => setShowPassword(!showPassword)} // Toggle visibility
-          >
+          <input type={showPassword ? 'text' : 'password'} placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <span className="toggle-password" onClick={() => setShowPassword(!showPassword)}> {/*Toggle Visibilty*/}
             {showPassword ? <FaEyeSlash /> : <FaEye />} {/* Show either eye or eye-slash icon */}
           </span>
         </div>
@@ -88,5 +73,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
