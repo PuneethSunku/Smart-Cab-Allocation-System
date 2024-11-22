d5852f2f4f34ef99194cdde00c2b971011454d0d
PuneethSunku <134520958+PuneethSunku@users.noreply.github.com>
2024-11-22 12:50:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,12 @@
 // client/backend/server.js
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const cabRoutes = require('./routes/cabRoutes');
-const placesRoute = require('./routes/placesRoute');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
 connectDB();
 
@@ -21,20 +16,9 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/cabs', cabRoutes); // Register cab routes
-app.use('/api', placesRoute);
 
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('cabLocationUpdate', (data) => {
-    io.emit('cabLocationUpdate', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Smart Cab backend listening on port ${PORT}`);
+})
